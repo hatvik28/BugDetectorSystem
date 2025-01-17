1fd5a22ffbd8ea215f077abcd319ee7ffaa78c78
hatvik28 <143751098+hatvik28@users.noreply.github.com>
2025-01-16 21:18:01 -0500

    Update BuggCode.java

--- BuggCode.java
+++ BuggCode.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BuggyCode {
+public class BuggCode {
 
     public static void main(String[] args) {
         // 1. Null Pointer Exception
