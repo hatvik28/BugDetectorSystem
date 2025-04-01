778277ad0867748adb3ca267d15554f0903e4a61
hatvik28 <hatvik28@gmail.com>
2025-03-31 22:00:54 -0400

    Automated bug fixes applied

--- BuggCode.java
+++ BuggCode.java
@@ -1,37 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class BuggCode {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        String str = null;
-        int length = str.length();
-        
+    String str = "Hello World";
+    int length = str.length();
+    System.out.println(length);
 
+    int[] numbers = {1, 2, 3};
+    System.out.println(numbers[3]);
 
-        int[] numbers = {1, 2, 3};
-        System.out.println(numbers[3]); 
+    Object obj = "hello";
+    Integer num = (Integer) obj;
 
-        
-
-        Object obj = "hello";
-        Integer num = (Integer) obj;
-
-        
-        int i = 0;
-        while (i >= 0) { 
-            System.out.println("Infinite Loop!");
-        }
-
-        
-
-        String text = "Buggy";
-        System.out.println(text.substring(10)); 
-
-
-        if (1 == 2) { 
-            System.out.println("This code is unreachable!");
-        }
+    int i = 0;
+    while (i >= 0) {
+      System.out.println("Infinite Loop!");
     }
+
+    String text = "Buggy";
+    System.out.println(text.substring(10));
+
+    if (1 == 2) {
+      System.out.println("This code is unreachable!");
+    }
+  }
 }
