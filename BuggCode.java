c742438ae3a1129c74d923344890a4f4135b19ad
hatvik28 <hatvik28@gmail.com>
2025-04-04 17:50:34 -0400

    Automated bug fixes applied

--- BuggCode.java
+++ BuggCode.java
@@ -1,29 +1,24 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-
+import javax.swing.*;
 
 public class BuggCode {
 
   public static void main(String[] args) {
 
-    String str = null;
+    String str = "Hello, World!";
     int length = str.length();
+    System.out.println("Length of the string: " + length);
 
     int[] numbers = {1, 2, 3};
-     
+
     System.out.println(numbers[2]);
-    
+
     int i = 0;
     while (i < 5) {
       System.out.println("Loop iteration: " + i);
       i++;
     }
- 
+
     String text = "Buggy";
     System.out.println(text);
 
