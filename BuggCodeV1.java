e64c2e8d2216d6bc349bca47094ef883fdd7e5b8
hatvik28 <hatvik28@gmail.com>
2025-04-09 15:15:27 -0400

    Automated bug fixes applied

--- BuggCodeV1.java
+++ BuggCodeV1.java
@@ -1,37 +1,25 @@
-import java.util.ArrayList;
- import java.util.List;
- 
- public class BuggCodeV1 {
- 
-     public static void main(String[] args) {
- 
-         String str = null;
-         int length = str.length();
- 
- 
- 
-         int[] numbers = {1, 2, 3};
-         System.out.println(numbers[3]); 
- 
- 
- 
-         Object obj = "hello";
-         Integer num = (Integer) obj;
- 
- 
-         int i = 0;
-         while (i >= 0) { 
-             System.out.println("Infinite Loop!");
-         }
- 
- 
- 
-         String text = "Buggy";
-         System.out.println(text.substring(10)); 
- 
- 
-         if (1 == 2) { 
-             System.out.println("This code is unreachable!");
-         }
-     }
- }
+public class BuggCodeV1 {
+
+  public static void main(String[] args) {
+
+    String str = null;
+    int length = (str != null) ? str.length() : 0;
+
+    int[] numbers = {1, 2, 3};
+    System.out.println(numbers[2]);
+
+    Object obj = "hello";
+    Integer num = (Integer) obj;
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Loop iteration: " + i);
+    }
+
+    String text = "Buggy";
+    System.out.println(text.substring(10));
+
+    if (1 == 2) {
+      System.out.println("This code is unreachable!");
+    }
+  }
+}
